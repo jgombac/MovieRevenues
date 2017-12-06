9be34d284bad9a028a7561b8d579146c3b06276b
jgom <jost.gombac@ineor.si>
2017-12-06 15:33:02 +0100

    method simplification

--- file_manager.py
+++ file_manager.py
@@ -2,24 +2,25 @@ import pandas as pd
 import ast
 
 MOVIES_FILEPATH = "movies_demo.csv"
+movies_json_cols = ["belongs_to_collection", "genres", "production_companies", "production_countries", "spoken_languages"]
+
+CREDITS_FILEPATH = "credits_demo.csv"
+credits_json_cols = ["cast", "crew"]
+
+KEYWORDS_FILEPATH = "keywords_demo.csv"
+keywords_json_cols = ["keywords"]
 
 
-def readFiles():
-    movies = pd.read_csv(MOVIES_FILEPATH)
-    json_cols = ["belongs_to_collection", "genres", "production_companies", "production_countries", "spoken_languages"]
+def read_file(filename, json_cols):
+    file = pd.read_csv(filename)
     for column in json_cols:
-        for i, x in enumerate(movies[column]):
+        for i, x in enumerate(file[column]):
             if pd.notna(x):
-                movies[column][i] = ast.literal_eval(x)
-    print(movies)
-
-    #vsak json stolpec locis v samostojno tabelo
-    #ga pobrises iz te tabele in vse zapises v bazo
-    #tej stolpci so ze transformirani v slovarje
-
-
-
+                file[column][i] = ast.literal_eval(x)
+    print(file)
 
 
 if __name__ == "__main__":
-    readFiles()
+    read_file(KEYWORDS_FILEPATH, keywords_json_cols)
+    read_file(CREDITS_FILEPATH, credits_json_cols)
+    read_file(MOVIES_FILEPATH, movies_json_cols)
