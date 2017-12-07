1f1da8d82eefe3c785818317edfcebedc7f3b631
jgom <jost.gombac@ineor.si>
2017-12-07 13:50:46 +0100

    er model,

---
+++ database_manager.py
@@ -0,0 +1,33 @@
+
+
+# stevilo unikatnih vrstic v podanih stolpcih in tabelah
+# vhod: dict {"ime_tabele1": ["stolpec1", "stolpec2"], "ime_tabele2": ["stolpec1"]}
+# izhod: int
+def num_unique(tables, columns):
+    pass
+
+# slovar imen stolpcev in pripadajoci podatkovni tipi vseh tabel
+# vhod: /
+# izhod: dict { "ime_tabele1": [["ime_stolpca1", "integer"], ["ime_stolpca2", "string"]], ...}
+def column_types():
+    pass
+
+
+
+def all_actors():
+    pass
+
+
+
+
+# id filma kjer nastopa igralec
+# vhod: "Ime Igralca"
+# izhod: int (movieId)
+def acts_in_movie(actor_id):
+    pass
+
+
+
+
+
+
--- predictor.py
+++ predictor.py
@@ -2,3 +2,25 @@ import tensorflow as tf
 import pandas as pd
 
 
+estimator = None
+
+def initialize():
+    feature_columns = []
+    hidden_units = [40, 60, 20]
+    estimator = tf.estimator.DNNRegressor(
+        feature_columns=feature_columns,
+        hidden_units=hidden_units,
+    )
+
+def build_train_input(dataframe):
+    pass
+
+def build_test_input(dataframe):
+    pass
+
+def train(input):
+    estimator.fit()
+
+def predict(input):
+    pass
+
