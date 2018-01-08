6aca5ba3fcb0d44d9d42f52cd030bc907cbae3b0
Jost Gombac <gombac.jost@gmail.com>
2018-01-08 21:25:49 +0100

    multithreading

--- database_manager.py
+++ database_manager.py
@@ -5,8 +5,12 @@
 
 import pyodbc as po
 import pandas as pd
+import numpy as np
 import time
+import tensorflow as tf
+from multiprocessing import Queue
 import threading
+import traceback
 
 class DB(object):
 
@@ -17,9 +21,9 @@ class DB(object):
     def close(self):
         self.connection.close()
 
-    def movies(self):
-        sql = "SELECT distinct ID_TMDB, (ID_COLLECTION IS NOT NULL) AS IN_COLLECTION, ORIGINAL_LANGUAGE, BUDGET, REVENUE FROM movies where REVENUE > 1000 and BUDGET > 1000"
-        return pd.read_sql(sql, self.connection)
+    def movies(self, offset):
+        sql = "SELECT distinct ID_TMDB, (ID_COLLECTION IS NOT NULL) AS IN_COLLECTION, ORIGINAL_LANGUAGE, BUDGET, REVENUE FROM movies where REVENUE > 1000 and BUDGET > 1000 limit ?, 10"
+        return pd.read_sql(sql, self.connection, params=[offset])
 
     def actors(self):
         sql = "select pl.ID_PERSON, pl.NAME from people as pl inner join " +\
@@ -33,11 +37,11 @@ class DB(object):
         sql = "select pl.ID_PERSON, pl.NAME from people as pl inner join " +\
                 "(select distinct cr.ID_PERSON from cast as ca inner join " +\
                 "(select cr.ID_CREDIT, cr.ID_PERSON from credits as cr inner join " +\
-                "(select mov.ID_TMDB from movies as mov where mov.ID_TMDB = "+id_tmdb+") " +\
+                "(select mov.ID_TMDB from movies as mov where mov.ID_TMDB = ?) " +\
                 "as mov on (cr.ID_TMDB = mov.ID_TMDB))" +\
                 "as cr on (ca.id_credit = cr.ID_CREDIT)) " +\
                 "as ca on (ca.ID_PERSON = pl.ID_PERSON)"
-        return pd.read_sql(sql, self.connection)
+        return self.cur.execute(sql, id_tmdb).fetchall()
 
     def directors(self):
         sql = "select pl.ID_PERSON, pl.NAME from people as pl inner join " +\
@@ -49,65 +53,150 @@ class DB(object):
                 "as ca on (ca.ID_PERSON = pl.ID_PERSON)"
         return pd.read_sql(sql, self.connection)
 
-    def movie_director(self, id_tmdb):
+    def movie_directors(self, id_tmdb):
         sql = "select pl.ID_PERSON, pl.NAME from people as pl inner join " +\
                 "(select distinct cr.ID_PERSON from crew as ca inner join " +\
                 "(select cr.ID_CREDIT, cr.ID_PERSON from credits as cr inner join " +\
-                "(select mov.ID_TMDB from movies as mov where mov.ID_TMDB = "+id_tmdb+") " +\
+                "(select mov.ID_TMDB from movies as mov where mov.ID_TMDB = ?) " +\
                 "as mov on (cr.ID_TMDB = mov.ID_TMDB))" +\
                 "as cr on (ca.id_credit = cr.ID_CREDIT) where ca.job = 'Director') " +\
-                "as ca on (ca.ID_PERSON = pl.ID_PERSON) limit 1"
-        return pd.read_sql(sql, self.connection)
+                "as ca on (ca.ID_PERSON = pl.ID_PERSON)"
+        return self.cur.execute(sql, id_tmdb).fetchall()
 
 
 MOVIES = None
 ACTORS = None
 DIRECTORS = None
 
-
-def movie_actors():
-    print("act start")
+def movies(offset, q):
     db = DB()
     try:
-        for i, val in MOVIES.iterrows():
-            if i > 10:
-                db.close()
-                print("act end")
-                return
-            actors = db.movie_actors(str(val["ID_TMDB"]))
+        print("fetch movies")
+        movies = db.movies(offset)
+        actors = movie_actors(movies)
+        #directors = movie_directors(movie_ids)
+
+        db.close()
+
+        cat_act = categorize_actors(actors)
+        #rint(cat_act)
+        movies.set_index("ID_TMDB", inplace=True)
+
+        joined = pd.concat([movies, cat_act], axis=1, join_axes=[movies.index])
+        q.put(joined)
+        return
     except:
         db.close()
-        print("act err")
+        print(traceback.print_exc())
 
-def movie_directors():
-    print("dir start")
+def movie_actors(movies):
     db = DB()
     try:
-        for i, val in MOVIES.iterrows():
-            if i > 10:
-                db.close()
-                print("dir end")
-                return
-            director = db.movie_director(str(val["ID_TMDB"]))
+        m_movies = pd.concat([movies[["ID_TMDB"]], pd.DataFrame({"actors":[None]})], axis=1)
+        #m_movies["actors"] = pd.Series()
+        for i, val in m_movies.iterrows():
+            actors = db.movie_actors(val["ID_TMDB"])
+            m_movies.ix[i, "actors"] = "|".join([x[0] for x in actors])
+        db.close()
+        print("act fetched")
+        return m_movies
     except:
         db.close()
-        print("dir err")
+        print("act err", traceback.print_exc())
+
+def movie_directors(movies):
+    db = DB()
+    try:
+        movies["directors"] = pd.Series()
+        for i, val in movies.iteritems():
+            directors = db.movie_directors(val)
+            movies["directors"][i] = "|".join([x[0] for x in directors])
+        db.close()
+        print("dir fetched")
+    except:
+        db.close()
+        print("dir err", traceback.print_exc())
+
+
+def categorize_actors(actors):
+    print("cat act")
+    data = actors[["ID_TMDB", "actors"]]
+    cleaned = data.set_index("ID_TMDB").actors.str.split("|", expand=True).stack()
+    cats = pd.get_dummies(cleaned, prefix='a').groupby(level=0).sum()
+    return cats
+    #print(cats)
+
+def categorize_directors(movies):
+    print("cat dir")
+    data = movies[["ID_TMDB", "directors"]]
+    cleaned = data.set_index("ID_TMDB").directors.str.split("|", expand=True).stack()
+    cats = pd.get_dummies(cleaned, prefix='a').groupby(level=0).sum()
+    print(cats)
+
+def movie_worker():
+    threads = []
+    q = Queue()
+    for i in range(0, 40, 10):
+        t = threading.Thread(target=movies, args=[i, q])
+        threads.append(t)
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    movie_data = None
+    for i in range(len(threads)):
+        result = q.get(i)
+        if movie_data is None:
+            movie_data = result
+        else:
+            movie_data = pd.concat([movie_data, result], ignore_index=True)
+
+    movie_data.fillna(value=0.0, inplace=True)
+    print(movie_data)
+
+
+    print("movies fetched")
+
+# def actor_worker():
+#     movie_actors()
+#     categorize_actors()
+#
+# def director_worker():
+#     movie_directors()
+#     categorize_directors()
+
 
 def init():
     global MOVIES, ACTORS, DIRECTORS
     db = DB()
     try:
-        t = time.time()
-        MOVIES = db.movies()
+        ti = time.time()
+        #MOVIES = db.movies()
         #ACTORS = db.actors()
         #DIRECTORS = db.directors()
-        print("MAIN DONE")
+        #print("MAIN DONE")
+        #transform_table(DIRECTORS)
 
-        at = threading.Thread(target=movie_actors)
-        dt = threading.Thread(target=movie_directors)
-        at.start()
-        dt.start()
-        print((time.time() - t), "s")
+        threads = []
+        mt = threading.Thread(target=movie_worker)
+        #at = threading.Thread(target=actor_worker)
+        #dt = threading.Thread(target=director_worker)
+        threads.append(mt)
+        #threads.append(at)
+        #threads.append(dt)
+        for t in threads:
+            t.start()
+
+        for t in threads:
+            t.join()
+
+        #MOVIES.set_index("ID_TMDB", inplace=True)
+        #MOVIES.drop(["actors", "directors"], axis=1, inplace=True)
+        #print(MOVIES)
+
+        print((time.time() - ti), "s")
         db.close()
     except Exception as e:
         print(e)
@@ -115,35 +204,4 @@ def init():
 
 if __name__ == "__main__":
     init()
-
-
-
-
-# stevilo unikatnih vrstic v podanih stolpcih in tabelah
-# vhod: dict {"ime_tabele1": ["stolpec1", "stolpec2"], "ime_tabele2": ["stolpec1"]}
-# izhod: int
-def num_unique(tables, columns):
-    pass
-
-# slovar imen stolpcev in pripadajoci podatkovni tipi vseh tabel
-# vhod: /
-# izhod: dict { "ime_tabele1": [["ime_stolpca1", "integer"], ["ime_stolpca2", "string"]], ...}
-def column_types():
-    pass
-
-
-# seznam vseh igralcev
-# vhod: /
-# izhod: list [{id_igralca1: "ime_igralca1"}, {id_igralca2: "ime_igralca2"}]
-def all_actors():
-    pass
-
-# seznam vseh kljucnih besed
-# vhod: /
-# izhod: list [{id_KB1: "besedilo_KB1"}, {id_KB2: "besedilo_KB2"}]
-def all_keywords():
-    pass
-
-
-
 
