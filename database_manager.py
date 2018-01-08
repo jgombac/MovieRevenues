ddba42095136a9346e1741a2678078e2f1493e9b
Jost Gombac <gombac.jost@gmail.com>
2018-01-08 22:46:26 +0100

    threading

--- database_manager.py
+++ database_manager.py
@@ -22,7 +22,7 @@ class DB(object):
         self.connection.close()
 
     def movies(self, offset):
-        sql = "SELECT distinct ID_TMDB, (ID_COLLECTION IS NOT NULL) AS IN_COLLECTION, ORIGINAL_LANGUAGE, BUDGET, REVENUE FROM movies where REVENUE > 1000 and BUDGET > 1000 limit ?, 10"
+        sql = "SELECT distinct ID_TMDB, (ID_COLLECTION IS NOT NULL) AS IN_COLLECTION, ORIGINAL_LANGUAGE, BUDGET, REVENUE FROM movies where REVENUE > 1000 and BUDGET > 1000 limit ?, 100"
         return pd.read_sql(sql, self.connection, params=[offset])
 
     def actors(self):
@@ -64,79 +64,66 @@ class DB(object):
         return self.cur.execute(sql, id_tmdb).fetchall()
 
 
-MOVIES = None
-ACTORS = None
-DIRECTORS = None
-
 def movies(offset, q):
     db = DB()
     try:
-        print("fetch movies")
         movies = db.movies(offset)
         actors = movie_actors(movies)
-        #directors = movie_directors(movie_ids)
-
+        directors = movie_directors(movies)
         db.close()
 
-        cat_act = categorize_actors(actors)
-        #rint(cat_act)
-        movies.set_index("ID_TMDB", inplace=True)
+        cat_act = categorize(actors, "actor")
+        cat_dir = categorize(directors, "director")
 
-        joined = pd.concat([movies, cat_act], axis=1, join_axes=[movies.index])
+        movies.set_index("ID_TMDB", inplace=True)
+        joined = pd.concat([movies, cat_act, cat_dir], axis=1, join_axes=[movies.index])
         q.put(joined)
         return
     except:
         db.close()
         print(traceback.print_exc())
 
+
 def movie_actors(movies):
     db = DB()
     try:
-        m_movies = pd.concat([movies[["ID_TMDB"]], pd.DataFrame({"actors":[None]})], axis=1)
-        #m_movies["actors"] = pd.Series()
+        m_movies = pd.concat([movies[["ID_TMDB"]], pd.DataFrame({"actor":[None]})], axis=1)
         for i, val in m_movies.iterrows():
             actors = db.movie_actors(val["ID_TMDB"])
-            m_movies.ix[i, "actors"] = "|".join([x[0] for x in actors])
+            m_movies.ix[i, "actor"] = "|".join([x[0] for x in actors])
         db.close()
-        print("act fetched")
         return m_movies
     except:
         db.close()
         print("act err", traceback.print_exc())
 
+
 def movie_directors(movies):
     db = DB()
     try:
-        movies["directors"] = pd.Series()
-        for i, val in movies.iteritems():
-            directors = db.movie_directors(val)
-            movies["directors"][i] = "|".join([x[0] for x in directors])
+        m_movies = pd.concat([movies[["ID_TMDB"]], pd.DataFrame({"director":[None]})], axis=1)
+        for i, val in m_movies.iterrows():
+            directors = db.movie_directors(val["ID_TMDB"])
+            m_movies.ix[i, "director"] = "|".join([x[0] for x in directors])
         db.close()
-        print("dir fetched")
+        return m_movies
     except:
         db.close()
-        print("dir err", traceback.print_exc())
+        print("act err", traceback.print_exc())
 
 
-def categorize_actors(actors):
-    print("cat act")
-    data = actors[["ID_TMDB", "actors"]]
-    cleaned = data.set_index("ID_TMDB").actors.str.split("|", expand=True).stack()
-    cats = pd.get_dummies(cleaned, prefix='a').groupby(level=0).sum()
+def categorize(data, column):
+    data = data[["ID_TMDB", column]]
+    cleaned = data.set_index("ID_TMDB")[column].str.split("|", expand=True).stack()
+    cats = pd.get_dummies(cleaned, prefix=column).groupby(level=0).sum()
     return cats
-    #print(cats)
 
-def categorize_directors(movies):
-    print("cat dir")
-    data = movies[["ID_TMDB", "directors"]]
-    cleaned = data.set_index("ID_TMDB").directors.str.split("|", expand=True).stack()
-    cats = pd.get_dummies(cleaned, prefix='a').groupby(level=0).sum()
-    print(cats)
+
 
 def movie_worker():
     threads = []
     q = Queue()
-    for i in range(0, 40, 10):
+    for i in range(0, 400, 100):
         t = threading.Thread(target=movies, args=[i, q])
         threads.append(t)
     for t in threads:
@@ -151,21 +138,12 @@ def movie_worker():
         if movie_data is None:
             movie_data = result
         else:
-            movie_data = pd.concat([movie_data, result], ignore_index=True)
+            movie_data = pd.concat([movie_data, result])
 
     movie_data.fillna(value=0.0, inplace=True)
     print(movie_data)
 
 
-    print("movies fetched")
-
-# def actor_worker():
-#     movie_actors()
-#     categorize_actors()
-#
-# def director_worker():
-#     movie_directors()
-#     categorize_directors()
 
 
 def init():
@@ -173,28 +151,13 @@ def init():
     db = DB()
     try:
         ti = time.time()
-        #MOVIES = db.movies()
-        #ACTORS = db.actors()
-        #DIRECTORS = db.directors()
-        #print("MAIN DONE")
-        #transform_table(DIRECTORS)
-
         threads = []
         mt = threading.Thread(target=movie_worker)
-        #at = threading.Thread(target=actor_worker)
-        #dt = threading.Thread(target=director_worker)
         threads.append(mt)
-        #threads.append(at)
-        #threads.append(dt)
         for t in threads:
             t.start()
-
         for t in threads:
             t.join()
-
-        #MOVIES.set_index("ID_TMDB", inplace=True)
-        #MOVIES.drop(["actors", "directors"], axis=1, inplace=True)
-        #print(MOVIES)
 
         print((time.time() - ti), "s")
         db.close()
